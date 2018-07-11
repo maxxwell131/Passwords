7e3d35023b2975ca9a24e888672df08353e07607
maxxwell131 <maxxwell131@yahoo.com>
2018-07-11 15:27:48 +0300

    finish commit

---
+++ Passwords/FormPassword.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Passwords
+{
+    partial class FormPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPassword));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonPassword3 = new System.Windows.Forms.Button();
+            this.textBoxLogin3 = new System.Windows.Forms.TextBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.buttonPassword2 = new System.Windows.Forms.Button();
+            this.textBoxLogin2 = new System.Windows.Forms.TextBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.buttonPassword1 = new System.Windows.Forms.Button();
+            this.textBoxLogin1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonPassword3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxLogin3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.buttonPassword2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxLogin2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonPassword1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxLogin1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 143);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonPassword3
+            // 
+            this.buttonPassword3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonPassword3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPassword3.Location = new System.Drawing.Point(526, 104);
+            this.buttonPassword3.Margin = new System.Windows.Forms.Padding(10);
+            this.buttonPassword3.Name = "buttonPassword3";
+            this.buttonPassword3.Size = new System.Drawing.Size(240, 29);
+            this.buttonPassword3.TabIndex = 8;
+            this.buttonPassword3.Tag = "zxcv";
+            this.buttonPassword3.Text = "Password";
+            this.buttonPassword3.UseVisualStyleBackColor = true;
+            this.buttonPassword3.Click += new System.EventHandler(this.buttonPassword1_Click);
+            // 
+            // textBoxLogin3
+            // 
+            this.textBoxLogin3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLogin3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLogin3.Location = new System.Drawing.Point(268, 104);
+            this.textBoxLogin3.Margin = new System.Windows.Forms.Padding(10);
+            this.textBoxLogin3.Multiline = true;
+            this.textBoxLogin3.Name = "textBoxLogin3";
+            this.textBoxLogin3.ReadOnly = true;
+            this.textBoxLogin3.Size = new System.Drawing.Size(238, 29);
+            this.textBoxLogin3.TabIndex = 7;
+            this.textBoxLogin3.Text = "myMail@facebook.com";
+            this.textBoxLogin3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxLogin3.Click += new System.EventHandler(this.textBoxLogin1_Click);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkLabel3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel3.Location = new System.Drawing.Point(10, 104);
+            this.linkLabel3.Margin = new System.Windows.Forms.Padding(10);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(238, 29);
+            this.linkLabel3.TabIndex = 6;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Tag = "www.facebook.com";
+            this.linkLabel3.Text = "www.facebook.com";
+            this.linkLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // buttonPassword2
+            // 
+            this.buttonPassword2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonPassword2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPassword2.Location = new System.Drawing.Point(526, 57);
+            this.buttonPassword2.Margin = new System.Windows.Forms.Padding(10);
+            this.buttonPassword2.Name = "buttonPassword2";
+            this.buttonPassword2.Size = new System.Drawing.Size(240, 27);
+            this.buttonPassword2.TabIndex = 5;
+            this.buttonPassword2.Tag = "asdf";
+            this.buttonPassword2.Text = "Password";
+            this.buttonPassword2.UseVisualStyleBackColor = true;
+            this.buttonPassword2.Click += new System.EventHandler(this.buttonPassword1_Click);
+            // 
+            // textBoxLogin2
+            // 
+            this.textBoxLogin2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLogin2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLogin2.Location = new System.Drawing.Point(268, 57);
+            this.textBoxLogin2.Margin = new System.Windows.Forms.Padding(10);
+            this.textBoxLogin2.Multiline = true;
+            this.textBoxLogin2.Name = "textBoxLogin2";
+            this.textBoxLogin2.ReadOnly = true;
+            this.textBoxLogin2.Size = new System.Drawing.Size(238, 27);
+            this.textBoxLogin2.TabIndex = 4;
+            this.textBoxLogin2.Text = "myMail@ukr.net";
+            this.textBoxLogin2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxLogin2.Click += new System.EventHandler(this.textBoxLogin1_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkLabel2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel2.Location = new System.Drawing.Point(10, 57);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(10);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(238, 27);
+            this.linkLabel2.TabIndex = 3;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Tag = "www.ukr.net";
+            this.linkLabel2.Text = "www.ukr.net";
+            this.linkLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linkLabel1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel1.Location = new System.Drawing.Point(10, 10);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(10);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(238, 27);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Tag = "http://google.com";
+            this.linkLabel1.Text = "www.google.com";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // buttonPassword1
+            // 
+            this.buttonPassword1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonPassword1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPassword1.Location = new System.Drawing.Point(526, 10);
+            this.buttonPassword1.Margin = new System.Windows.Forms.Padding(10);
+            this.buttonPassword1.Name = "buttonPassword1";
+            this.buttonPassword1.Size = new System.Drawing.Size(240, 27);
+            this.buttonPassword1.TabIndex = 1;
+            this.buttonPassword1.Tag = "qwerty";
+            this.buttonPassword1.Text = "Password";
+            this.buttonPassword1.UseVisualStyleBackColor = true;
+            this.buttonPassword1.Click += new System.EventHandler(this.buttonPassword1_Click);
+            // 
+            // textBoxLogin1
+            // 
+            this.textBoxLogin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxLogin1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLogin1.Location = new System.Drawing.Point(268, 10);
+            this.textBoxLogin1.Margin = new System.Windows.Forms.Padding(10);
+            this.textBoxLogin1.Multiline = true;
+            this.textBoxLogin1.Name = "textBoxLogin1";
+            this.textBoxLogin1.ReadOnly = true;
+            this.textBoxLogin1.Size = new System.Drawing.Size(238, 27);
+            this.textBoxLogin1.TabIndex = 2;
+            this.textBoxLogin1.Text = "myMail@mail.com";
+            this.textBoxLogin1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxLogin1.Click += new System.EventHandler(this.textBoxLogin1_Click);
+            // 
+            // FormPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 143);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Passwords";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button buttonPassword1;
+        private System.Windows.Forms.TextBox textBoxLogin1;
+        private System.Windows.Forms.Button buttonPassword3;
+        private System.Windows.Forms.TextBox textBoxLogin3;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.Button buttonPassword2;
+        private System.Windows.Forms.TextBox textBoxLogin2;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ Passwords/FormPassword.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Passwords
+{
+    public partial class FormPassword : Form
+    {
+        public FormPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start((String)((LinkLabel)sender).Tag);
+        }
+
+        private void textBoxLogin1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(((TextBox)sender).Text);
+            ((TextBox)sender).SelectAll();
+        }
+
+        private void buttonPassword1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText((string)((Button)sender).Tag);
+            Close();
+        }
+    }
+}
